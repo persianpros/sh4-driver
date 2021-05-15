e53dcfb5cd0b6de6045eda2aae0ba46aeca1299f
Persian Prince <persianpros@yahoo.com>
2021-05-15 12:51:20 +0430

    Tuner debug fixes by Audioniek

--- frontends/multituner/frontends/tda10023/core.c
+++ frontends/multituner/frontends/tda10023/core.c
@@ -286,6 +286,6 @@ module_param(paramDebug, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
 MODULE_PARM_DESC(paramDebug, "Debug Output 0=disabled >0=enabled(debuglevel)");
 
 MODULE_DESCRIPTION ("Tuner driver");
-MODULE_AUTHOR      ("konfetti");
+MODULE_AUTHOR      ("Open Vision developers");
 MODULE_LICENSE     ("GPL");
 // vim:ts=4
